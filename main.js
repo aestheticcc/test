c7e0d713364e193b3d780e886a5b039c11f70ad3
aestheticcc <47005844+aestheticcc@users.noreply.github.com>
2019-04-16 20:44:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+let listObject = {
+    test:false,
+    test2:false,
+    test3:false,
+    test4:false,
+}
+let listElem = document.getElementById("list");
+let addButton = document.getElementById("addButton");
+let textBox = document.getElementById("text");
+addButton.addEventListener("click", function(){addItem(textBox.value);}, false);
+
+function addItem(itemName){
+    if(itemName != "" && listObject[itemName] === undefined){
+        listObject[itemName] = true;
+        console.log("added " + itemName);
+        textBox.value = "";
+        refreshList();
+    }
+}
+function removeItem(itemName){
+    delete listObject[itemName];
+    console.log("removed " + itemName);
+    refreshList();
+}
+function createItemElement(itemName){
+    let mainElem = document.createElement("li");
+    let textElem = document.createElement("p");
+    let delButton = document.createElement("button");
+    delButton.innerHTML = "Delete";
+    delButton.addEventListener("click", function(){removeItem(itemName);
+    }, false);
+    //mainElem.id = itemName;
+    textElem.innerHTML = itemName;
+    mainElem.appendChild(textElem);
+    mainElem.appendChild(delButton);
+
+    return mainElem;
+}
+function refreshList(){
+
+    /*
+    This thing doesn't work for some reason
+
+    let items = listElem.childNodes;
+    items.forEach(element => {
+        listElem.removeChild(element);
+    });
+    */
+    while (listElem.firstChild){
+        listElem.removeChild(listElem.firstChild)
+    }
+    for(let key in listObject){
+        console.log(key);
+        listElem.appendChild(createItemElement(key));
+    
+    }
+    console.log(listElem.childNodes);
+}
+refreshList();
